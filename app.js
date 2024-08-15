1768820abf6b6abd3787f8a51cb99dc663ab0abd
Evangelina Vega <evangelina1001@hotmail.com>
2024-08-15 16:00:29 -0700

    Added: Horizontal Scrolling

--- app.js
+++ app.js
@@ -24,15 +24,36 @@ const sectionTwoTl = gsap.timeline({
     scrollTrigger:{
         trigger: ".section2",
         start: "top 25%", 
-        end: "bottom center"
+        end: "bottom center",
+        toggleActions: "restart none reverse"
     }
 })
 
 sectionTwoTl.from(".section2 div h2 span", {y: "-100vh", stagger: .2})
             .from(".section2 div p", {rotationY: 90, opacity: 0})
 
+//HORIZONTAL SECTIONS
 
+const container = document.querySelector(".horizontal-sections")
+let containerWidth = container.offsetWidth
+let amountToScroll = containerWidth - window.innerWidth
 
+const sectionHorizontalTl = gsap.timeline({
+    scrollTrigger:{
+        trigger: ".horizontal-sections",
+        markers: true, 
+        start: "top", 
+        end: "+=" + amountToScroll, 
+        pin: true, 
+        scrub: true, 
+    }
+})
+
+sectionHorizontalTl.from(".section3 div h2", {x: "-100vw", scale: 10})
+                .to(".section3 div p span", {backgroundColor: "white", color: "black"})
+                .from(".section3 div p span", {y: "100vh", stagger: .2})
+                .to(container, {x: -amountToScroll})
+                .from(".section4 .portfolio-item", {x:"100vw", opacity: 0})
 
 
 //Section five timeline 
@@ -40,28 +61,22 @@ sectionTwoTl.from(".section2 div h2 span", {y: "-100vh", stagger: .2})
 const sectionFiveTl = gsap.timeline({
     scrollTrigger:{
         trigger: ".section5",
-        markers: true,
         start: "top", 
         end: "bottom", 
         pin: true, 
         scrub: true, 
     }
 })
-
+//Separating contact me into each character 
 let contactMe = document.querySelector(".section5 h2")
 let textContact = contactMe.innerHTML.split("");
 let result = " "
-
 textContact.forEach((word,i) => {
     result +=`<span> ${word} </span>`
 })
-
 contactMe.innerHTML = result
-
 const emptyString = document.querySelector(".section5 h2 span:nth-child(8)")
 emptyString.innerHTML =  `&nbsp;`
-
-
 
 sectionFiveTl.from(".section5 h2 span", {y:"-100vh", stagger: .2})
             .from(".section5 div p", {y: 40, opacity: 0})
